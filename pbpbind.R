43cd48a59882cedd320e7a51754466855ca7930c
Colin <colinrwelsh@gmail.com>
2019-09-18 22:52:49 -0400

    pbpbind

---
+++ pbpbind.R
@@ -0,0 +1,17 @@
+userYear <- 2019
+userWeek <- 2
+
+g <- read.csv(paste("data/games_data/reg_season/reg_games_", userYear, ".csv", sep = ""))
+g <- g %>% filter(state_of_game == "POST")
+id <- g %>% pull(game_id)
+
+new_plays <- NULL
+for (x in id) {
+  game_plays <- read.csv(paste("data/games_data/", userYear, "/", x, ".csv", sep = ""))
+  new_plays <- bind_rows(new_plays,game_plays)
+}
+
+write.csv(new_plays, paste("data/games_data/", userYear, "/pbp", userYear, ".csv", sep = ""))
+
+
+read.csv(paste("data/games_data/", userYear, "/", sep = "")
