21ffb98e9a69f07ec1fab1c6082e577f74a0697f
Colin <colinrwelsh@gmail.com>
2019-09-23 22:59:54 -0400

    live pbp w plots

--- scrapepbp.R
+++ scrapepbp.R
@@ -50,6 +50,8 @@ for (x in games_in_play) {
     print(paste("Scraping game ", x, sep = ""))
     y <- scrape_json_play_by_play(x)
     write.csv(y, file = paste("data/games_data/", userYear,"/", x, ".csv", sep = ""))
+    print("Last play:")
+    print(y$desc[nrow(y)])
     endGame <- grepl("END GAME", y$desc)
     #check for end game and add post status if scrape includes
     if(any(endGame == TRUE)) {
@@ -59,7 +61,65 @@ for (x in games_in_play) {
   }
 }
   
-  
+
+
   
 ##  game_ids[game_ids$game_id == 2019092300, "state_of_game"] <- "PRE"
 ##  write.csv(game_ids, "data/games_data/reg_season/reg_games_2019.csv")
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+
+  
+  
+  
+  # Pull out the Steelers and Chief colors:
+  nfl_teamcolors <- teamcolors %>% filter(league == "nfl")
+  chi_color <- nfl_teamcolors %>%
+    filter(name == "Chicago Bears") %>%
+    pull(primary)
+  was_color <- nfl_teamcolors %>%
+    filter(name == "Washington Redskins") %>%
+    pull(primary)
+  
+  # Now generate the win probability chart:
+  y %>%
+    filter(!is.na(home_wp),
+           !is.na(away_wp)) %>%
+    dplyr::select(game_seconds_remaining,
+                  home_wp,
+                  away_wp) %>%
+    gather(team, wpa, -game_seconds_remaining) %>%
+    ggplot(aes(x = game_seconds_remaining, y = wpa, color = team)) +
+    geom_line(size = 2) +
+    geom_hline(yintercept = 0.5, color = "gray", linetype = "dashed") +
+    scale_color_manual(labels = c("CHI", "WAS"),
+                       values = c(chi_color, was_color),
+                       guide = FALSE) +
+    scale_x_reverse(breaks = seq(0, 3600, 300)) + 
+    annotate("text", x = 3000, y = .75, label = "CHI", color = chi_color, size = 8) + 
+    annotate("text", x = 3000, y = .25, label = "WAS", color = was_color, size = 8) +
+    geom_vline(xintercept = 900, linetype = "dashed", black) + 
+    geom_vline(xintercept = 1800, linetype = "dashed", black) + 
+    geom_vline(xintercept = 2700, linetype = "dashed", black) + 
+    geom_vline(xintercept = 0, linetype = "dashed", black) + 
+    labs(
+      x = "Time Remaining (seconds)",
+      y = "Win Probability",
+      title = paste("Week", userWeek, "Win Probability Chart", sep = " "),
+      subtitle = "Chicago Bears vs. Washington Redskins",
+      caption = "Data from nflscrapR"
+    ) + theme_bw()
+  
+  
+  print("Last play:")
+  print(y$desc[nrow(y)])
+  
