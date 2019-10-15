1f35d90fd84685e2a2985d80e25dcf3753fefe5a
Colin <colinrwelsh@gmail.com>
2019-10-15 00:05:09 -0400

    mnf 10/14

--- loop_yplot.R
+++ loop_yplot.R
@@ -1,4 +1,4 @@
-x2 <- 2019101308
+x2 <- 2019101400
 y2 <- scrape_json_play_by_play(x2)
 f2 <- paste("data/games_data/", userYear, "/", x2, ".csv", sep = "")
 write.csv(y2, f2)
@@ -24,12 +24,21 @@ awayTeam_logo <- nfl_teamcolors$logo[awayTeamInt]
   # note: Pull out the Home and Away colors:
   # note: Make this dynamic across games and add to loop
 nfl_teamcolors <- teamcolors %>% filter(league == "nfl")
+
 awayTeam_color <- nfl_teamcolors %>%
   filter(name == awayTeam_fullname) %>%
+  pull(secondary)
+awayTeam_secondarycolor <- nfl_teamcolors %>%
+  filter(name == awayTeam_fullname) %>%
   pull(primary)
+
 homeTeam_color <- nfl_teamcolors %>%
   filter(name == homeTeam_fullname) %>%
   pull(primary)
+homeTeam_secondarycolor <- nfl_teamcolors %>%
+  filter(name == homeTeam_fullname) %>%
+  pull(secondary)
+
     ##
     ##
   
@@ -41,18 +50,19 @@ y2 %>%
                 home_wp) %>%
   gather(team, wpa, -game_seconds_remaining) %>%
   ggplot(aes(x = game_seconds_remaining, y = wpa, color = team)) +
-  geom_line(size = 2) +
+  geom_line(size = 3) +
   geom_hline(yintercept = 0.5, color = "gray", linetype = "dashed") +
   scale_color_manual(labels = c(awayTeam_abbr, homeTeam_abbr),
                      values = c(awayTeam_color, homeTeam_color),
                      guide = FALSE) +
   scale_x_reverse(breaks = seq(0, 3600, 300)) + 
-  annotate("text", x = 3000, y = .75, label = awayTeam_abbr, color = awayTeam_color, size = 8) + 
-  annotate("text", x = 3000, y = .25, label = homeTeam_abbr, color = homeTeam_color, size = 8) +
+  annotate("text", x = 3000, y = .75, label = awayTeam_abbr, color = awayTeam_secondarycolor, size = 16) + 
+  annotate("text", x = 3000, y = .25, label = homeTeam_abbr, color = homeTeam_secondarycolor, size = 16) +
   geom_vline(xintercept = 900, linetype = "dashed", black) + 
   geom_vline(xintercept = 1800, linetype = "dashed", black) + 
   geom_vline(xintercept = 2700, linetype = "dashed", black) + 
   geom_vline(xintercept = 0, linetype = "dashed", black) + 
+#  geom_vline(xintercept = , linetype = "solid", black)
   labs(
     x = "Time Remaining (seconds)",
     y = "Win Probability",
@@ -60,3 +70,11 @@ y2 %>%
     subtitle = paste(awayTeam_fullname, "vs.", homeTeam_fullname, sep = " "),
     caption = "Data from nflscrapR"
   ) + theme_bw()
+
+print("Last play:")
+print(paste("EPA Added:", y2$epa[nrow(y2)-2], ",", y2$desc[nrow(y2)-2], sep = " "))
+print(paste("EPA Added:", y2$epa[nrow(y2)-1], ",", y2$desc[nrow(y2)-1], sep = " "))
+print(paste("EPA Added:", y2$epa[nrow(y2)], ",", y2$desc[nrow(y2)], sep = " "))
+
+print(paste(awayTeam_fullname, "Win Probability:", y2$away_wp[nrow(y2)], sep=" "))
+print(paste(homeTeam_fullname, "Win Probability:", y2$home_wp[nrow(y2)], sep = " "))
