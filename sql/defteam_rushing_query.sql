10d3f0557afe48c8cdc39c199cb81fb81565a348
Colin <colinrwelsh@gmail.com>
2021-08-15 22:44:15 -0400

    updated sql queries

--- sql/defteam_rushing_query.sql
+++ sql/defteam_rushing_query.sql
@@ -1,4 +1,5 @@
 SELECT defteam AS team, 
+season,
 COUNT(DISTINCT game_id) AS games,
 COUNT(game_id) AS rushes_allowed,
 COUNT(game_id) / COUNT(DISTINCT game_id) AS rush_attempts_allowed_pg,
@@ -7,6 +8,5 @@ SUM(yards_gained) / COUNT(DISTINCT game_id) AS rush_yards_allowed_pg,
 SUM(rush_touchdown) AS tot_rush_td_allowed, 
 SUM(rush_touchdown) /  COUNT(DISTINCT game_id) AS rush_td_allowed_pg
 FROM "nflfastR_pbp"
-WHERE (season = 2020.0 AND rush_attempt = 1.0 AND season_type = 'REG' AND two_point_attempt = 0.0 AND NOT(((rusher_player_id) IS NULL)))
-GROUP BY team
-ORDER BY rush_td_allowed DESC;
+WHERE (rush_attempt = 1.0 AND season_type = 'REG' AND two_point_attempt = 0.0 AND NOT(((rusher_player_id) IS NULL)))
+GROUP BY season, team;
