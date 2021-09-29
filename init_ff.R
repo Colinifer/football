7db53f290d7c5201a7adbaf352bd8188da01017a
Colin Welsh <colinrwelsh@gmail.com>
2021-09-29 00:01:58 -0400

    Update init_ff.R

--- init_ff.R
+++ init_ff.R
@@ -47,8 +47,37 @@ fantasy_rosters <- ff_rosters(ff_conn_beep_boop) %>%
                  league = 'Kepler')) %>%
   rbind(ff_rosters(ff_conn_family) %>%
           mutate(on_roster = TRUE,
-                 league = 'Family'))
-  
+                 league = 'Family')) %>% 
+  left_join(roster_df %>% 
+              select(
+                gsis_id,
+                espn_id),
+                by = c('player_id' = 'espn_id'))
+
+player_stats %>%
+  left_join(
+    fantasy_rosters %>% 
+      filter(league == 'Drinkers' & 
+               pos %in% c('RB', 'WR', 'TE')) %>% 
+      select(on_roster,franchise_name, gsis_id),
+    by = c('player_id' = 'gsis_id')
+  ) %>% 
+  left_join(
+    roster_df %>% 
+      select(
+        season,
+        position,
+        gsis_id
+      ),
+    by = c('player_id' = 'gsis_id')
+  ) %>% 
+  filter(position %in% c('RB', 'WR', 'TE')) %>% 
+  select(
+    season, on_roster, franchise_name, player_id:recent_team, position, offense_snaps:offense_pct, contains('rushing'), hvt, receptions, targets, contains('receiving'), racr, target_share, air_yards_share, wopr, contains('fantasy')
+  ) %>% 
+  filter(is.na(on_roster) | (franchise_name %in% c('Matt Rhules', 'Golden Rhule', 'Rhule Tide', 'Team Welsh'))) %>% 
+  arrange(-hvt)
+
 
 # Schedules
 ff_schedule_beep_boop <- ff_schedule(ff_conn_beep_boop)
